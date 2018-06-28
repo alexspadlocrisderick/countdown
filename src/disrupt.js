b774156b38c6d535424aa042aa588ce9fae27651
Cristian Derick <communication-world@live.com>
2018-06-28 14:29:15 -0300

    New stage

--- src/disrupt.js
+++ src/disrupt.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   //jQuery time
-  var current_fs, next_fs, previous_fs; //fieldsets
+  var current_fs, next_fs; //fieldsets
   var left, opacity, scale; //fieldset properties which we will animate
   var animating; //flag to prevent quick multi-click glitches
 
@@ -41,52 +41,16 @@ $(document).ready(function() {
       easing: 'easeInOutBack'
     });
   });
-
-  $(".previous").click(function(){
-    if(animating) return false;
-    animating = true;
-    
-    current_fs = $(this).parent();
-    previous_fs = $(this).parent().prev();
-    
-    //de-activate current step on progressbar
-    $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-    
-    //show the previous fieldset
-    previous_fs.show(); 
-    //hide the current fieldset with style
-    current_fs.animate({opacity: 0}, {
-      step: function(now, mx) {
-        //as the opacity of current_fs reduces to 0 - stored in "now"
-        //1. scale previous_fs from 80% to 100%
-        scale = 0.8 + (1 - now) * 0.2;
-        //2. take current_fs to the right(50%) - from 0%
-        left = ((1-now) * 50)+"%";
-        //3. increase opacity of previous_fs to 1 as it moves in
-        opacity = 1 - now;
-        current_fs.css({'left': left});
-        previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
-      }, 
-      duration: 800, 
-      complete: function(){
-        current_fs.hide();
-        animating = false;
-      }, 
-      //this comes from the custom easing plugin
-      easing: 'easeInOutBack'
-    });
-  });
-
-  $(".submit").click(function(){
-    return false;
-  })
 });
 
 
+setTimeout(function (){
+  $(".next").click();
+}, 2000);
 
 
 // set the date we're counting down to
-var target_date = new Date(2018, 05, 28, 10, 00, 00).getTime();
+var target_date = new Date(2018, 06, 26, 09, 00, 00).getTime();
 // variables for time units
 var days, hours, minutes, seconds;
 // update the tag with id "countdown" every 1 second
